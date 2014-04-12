984b690c1ec48f30c86a00c5ed2e2b501fd7182b
ivey <iveysaurrr@gmail.com>
2014-04-11 20:21:05 -0400

    Each connection has a different hand

--- backend/backend.js
+++ backend/backend.js
@@ -1,7 +1,7 @@
 var app = require('http').createServer(handler), io = require('socket.io').listen(app), fs = require('fs');
 var url = require('url');
 var questionList = require('./black'), responseList = require('./white');
-var curquestion, responses = [], hands = [];
+var curquestion, responses = [];
 var i, count = 0, played = [];
 
 app.listen(5700);
@@ -37,13 +37,14 @@ shuffle(responseList);
 
 curquestion = questionList.pop();
 
-for(i = 0; i < 7; i++){
-	responses.push(responseList.pop());
-}
 
 io.sockets.on('connection', function(socket){
 	socket.emit('question', curquestion);
-	socket.emit('player', responses);
+	responses.push([]);
+	for(i = 0; i < 7; i++){
+		responses[responses.length - 1].push(responseList.pop());
+	}
+	socket.emit('player', responses[responses.length - 1]);
 	socket.on('played', function(data){
 		console.log(data);
 		played.push(data);
