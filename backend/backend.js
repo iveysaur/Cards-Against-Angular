a75e2d95ae59f909ae06cde3665c7379d491426a
ivey <iveysaurrr@gmail.com>
2014-04-12 01:47:39 -0400

    Got judge stuff working

--- backend/backend.js
+++ backend/backend.js
@@ -1,8 +1,8 @@
 var app = require('http').createServer(handler), io = require('socket.io').listen(app), fs = require('fs');
 var url = require('url');
 var questionList = require('./black'), responseList = require('./white');
-var curquestion, responses = [];
-var i, count = 0, judge = 0, played = [];
+var curquestion, responses = [], players = [];
+var i, count = 0, judge = 0, judger, played = [];
 
 app.listen(5700);
 
@@ -37,28 +37,30 @@ shuffle(responseList);
 
 curquestion = questionList.pop();
 
-
 io.sockets.on('connection', function(socket){
 	socket.emit('question', curquestion);
 	count++;
 	judge++;
+	players.push(socket.id);
 	responses.push([]);
 	for(i = 0; i < 7; i++){
 		responses[responses.length - 1].push(responseList.pop());
 	}
 	socket.emit('player', responses[responses.length - 1]);
-	socket.emit('judge', judge%count);
+	judger = players[judge%players.length]; 
+	io.sockets.emit('judge', 0);
+	io.sockets.socket(judger).emit('judge', 1);
 	socket.on('played', function(data){
 		played.push(data);
 		socket.emit('new', responseList.pop());
-		if(played.length == count){
+		if(played.length == count - 1){
 			io.sockets.emit('playedlist', played);
 		}
 	});
 	socket.on('disconnect', function(data){
 		count--;
-		judge--;
 	});
 	console.log("++count: " + count);
+	console.log("++id: " + socket.id);
 });
 
--- cards.js
+++ cards.js
@@ -8,14 +8,22 @@ function QuestionCtrl($scope){
 
 function ResponseCtrl($scope){
 	$scope.responses = [];
-	$scope.judge;
+	$scope.judge = 2;
 	
 	$scope.remove = function(index){
-		if($scope.responses.length == 7){
+		if($scope.responses.length == 7 && $scope.judge != 1){
 			var played = $scope.responses[index];
 			$scope.responses.splice(index, 1);
 			socket.emit('played', played);
 		}
+	}
+
+
+	if($scope.judge == 1){
+		console.log("judge is 1!!!");
+	}
+	else{
+		console.log("judge in scope: " + $scope.judge);
 	}
 
 	responseScope = $scope;
@@ -51,6 +59,8 @@ socket.on('playedlist', function(data){
 socket.on('judge', function(data){
 	responseScope.$apply(function(){
 		responseScope.judge = data;
+		console.log("judge: " + data);
+		console.log("responseScope.judge: " + responseScope.judge);
 	});
 });
 
